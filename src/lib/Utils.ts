e616d9826e80cc93ff60b30c426b65e8b50b69a0
Lalo <gonzalo.alvalon@gmail.com>
2024-02-13 23:59:50 +0100

    Iprove SlugName function to handle special characters

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,7 +1,14 @@
 import { Type, type Role } from './ManfredType';
 
+// Credits: https://gist.github.com/max10rogerio/c67c5d2d7a3ce714c4bc0c114a3ddc6e
 export function SlugName(name: string) {
-  return name.trim().replaceAll(" ", "-").toLowerCase();
+  return name
+        .normalize('NFD') 
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9 ]/g, '')
+        .replace(/\s+/g, '-')
 }
 
 export function GetTechnologiesFromRoles(roles: Role[]) {
