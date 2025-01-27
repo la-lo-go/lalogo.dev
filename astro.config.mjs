93a4a14aa3bef7b79d5a0a0dc2283cca591c2a79
Lalo <gonzalo.alvalon@gmail.com>
2025-01-27 18:19:41 +0100

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://lalogo.dev",
+  integrations: [tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
